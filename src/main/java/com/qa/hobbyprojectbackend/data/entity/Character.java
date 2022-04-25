02540af65ac4f713c513984dd4e622ae2eef7f54
Callum <callumjrobinson2020@gmail.com>
2022-04-25 16:09:23 +0100

    Added id field with appropriate annotations

--- src/main/java/com/qa/hobbyprojectbackend/data/entity/Character.java
+++ src/main/java/com/qa/hobbyprojectbackend/data/entity/Character.java
@@ -1,10 +1,18 @@
 package com.qa.hobbyprojectbackend.data.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "character")
 public class Character {
 
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
 }
