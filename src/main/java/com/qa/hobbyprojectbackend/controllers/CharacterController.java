633e5b608910dbf607568a60db68ca53159a6a38
Callum <callumjrobinson2020@gmail.com>
2022-04-25 21:26:13 +0100

    Created character controller class

---
+++ src/main/java/com/qa/hobbyprojectbackend/controllers/CharacterController.java
@@ -0,0 +1,12 @@
+package com.qa.hobbyprojectbackend.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/user")
+@CrossOrigin("*")
+public class CharacterController {
+
+}
