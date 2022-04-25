2f7819fd5e11c2bee22d82c151ec276fe0eaba03
Callum <callumjrobinson2020@gmail.com>
2022-04-25 16:15:47 +0100

    Added race field to character with approriate constraints

--- src/main/java/com/qa/hobbyprojectbackend/data/entity/Character.java
+++ src/main/java/com/qa/hobbyprojectbackend/data/entity/Character.java
@@ -6,6 +6,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "character")
@@ -15,4 +18,9 @@ public class Character {
 	@Column(name = "id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
+	@NotNull (message = "Race cannot be null")
+	@NotBlank (message = "Race cannot be empty")
+	@Size(min = 3, max = 20, message = "Race needs a minimum of 3 characters and maximum of 20")
+	private String race;
 }
