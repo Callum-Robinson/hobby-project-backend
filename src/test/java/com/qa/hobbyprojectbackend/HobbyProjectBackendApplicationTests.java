b468e6edd32bb9791df6e97dab0de9e057a5cf8c
Callum <callumjrobinson2020@gmail.com>
2022-04-25 15:53:18 +0100

    Initial Setup

---
+++ src/test/java/com/qa/hobbyprojectbackend/HobbyProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.hobbyprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
