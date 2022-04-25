e6648f15802cdc254536d30af93441b4003cf23f
Callum <callumjrobinson2020@gmail.com>
2022-04-25 20:18:39 +0100

    Added to DTO method and get characters method to character service

--- src/main/java/com/qa/hobbyprojectbackend/service/CharacterService.java
+++ src/main/java/com/qa/hobbyprojectbackend/service/CharacterService.java
@@ -1,10 +1,15 @@
 package com.qa.hobbyprojectbackend.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.qa.hobbyprojectbackend.data.entity.MyCharacter;
 import com.qa.hobbyprojectbackend.data.repository.CharacterRepository;
+import com.qa.hobbyprojectbackend.dto.MyCharacterDTO;
 
 @Service
 public class CharacterService {
@@ -17,5 +22,20 @@ public class CharacterService {
 		super();
 		this.characterRepository = characterRepository;
 		this.modelMapper = modelMapper;
+	}
+	
+	private MyCharacterDTO toDTO(MyCharacter character) {
+		return this.modelMapper.map(character, MyCharacterDTO.class);
+	}
+	
+	public List<MyCharacterDTO> getCharacters() {
+		List<MyCharacter> characters = characterRepository.findAll();
+		List<MyCharacterDTO> dtos = new ArrayList<>();
+		
+		for (MyCharacter character : characters) {
+			dtos.add(this.toDTO(character));
+		}
+		
+		return dtos;
 	}
 }
