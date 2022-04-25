400df8d3a2985523460dac0f9cf1d76139cd9415
Callum <callumjrobinson2020@gmail.com>
2022-04-25 21:42:18 +0100

    added constructor for character controller

--- src/main/java/com/qa/hobbyprojectbackend/controllers/CharacterController.java
+++ src/main/java/com/qa/hobbyprojectbackend/controllers/CharacterController.java
@@ -1,12 +1,21 @@
 package com.qa.hobbyprojectbackend.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.hobbyprojectbackend.service.CharacterService;
 
 @RestController
 @RequestMapping(path = "/user")
 @CrossOrigin("*")
 public class CharacterController {
 
+	private CharacterService characterService;
+	
+	@Autowired
+	public CharacterController(CharacterService characterService) {
+		this.characterService = characterService;
+	}
 }
