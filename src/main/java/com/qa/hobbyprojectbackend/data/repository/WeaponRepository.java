d8bdf37279281e4f7dd95f3fc7e3488351b2dfbb
Callum <callumjrobinson2020@gmail.com>
2022-04-26 12:50:34 +0100

    added find weapon by character id method to weapon repository

--- src/main/java/com/qa/hobbyprojectbackend/data/repository/WeaponRepository.java
+++ src/main/java/com/qa/hobbyprojectbackend/data/repository/WeaponRepository.java
@@ -1,11 +1,12 @@
 package com.qa.hobbyprojectbackend.data.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.qa.hobbyprojectbackend.data.entity.Weapon;
 
-@Repository
 public interface WeaponRepository extends JpaRepository<Weapon, Integer> {
 
+	List<Weapon> findByMyCharacterId(int id);
 }
