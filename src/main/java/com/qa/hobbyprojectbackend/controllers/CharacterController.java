db17c29a4fd66b3afc35f0c7db02d75b4de34919
Callum <callumjrobinson2020@gmail.com>
2022-04-26 09:43:00 +0100

    added get mapping for read by id to character controller

--- src/main/java/com/qa/hobbyprojectbackend/controllers/CharacterController.java
+++ src/main/java/com/qa/hobbyprojectbackend/controllers/CharacterController.java
@@ -3,8 +3,11 @@ package com.qa.hobbyprojectbackend.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,10 +27,15 @@ public class CharacterController {
 	}
 	
 	// get mapping for read all
+	@GetMapping
 	public ResponseEntity<List<MyCharacterDTO>> getCharacters() {
 		return ResponseEntity.ok(characterService.getCharacters());
 	}
 	
 	// get mapping for read by character id
-	
+	@GetMapping(path = "/{id)")
+	public ResponseEntity<MyCharacterDTO> getCharacter(@PathVariable(name = "id") int id) {
+		MyCharacterDTO character = characterService.getCharacter(id);
+		return new ResponseEntity<>(character, HttpStatus.OK);
+	}
 }
