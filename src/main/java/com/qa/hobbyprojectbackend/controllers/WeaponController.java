6a1600551d45fe04027e8a65c64f5c5e66dd7a0d
Callum <callumjrobinson2020@gmail.com>
2022-04-26 17:50:13 +0100

    added delete mapping to weapon controller

--- src/main/java/com/qa/hobbyprojectbackend/controllers/WeaponController.java
+++ src/main/java/com/qa/hobbyprojectbackend/controllers/WeaponController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,5 +62,13 @@ public class WeaponController {
 	@PutMapping(path = "/{id}")
 	public ResponseEntity<WeaponDTO> updateWeapon(@Valid @RequestBody UpdateWeaponDTO weapon, @PathVariable(name = "id") int id) {
 		return ResponseEntity.ok(weaponService.updateWeapon(weapon, id));
+	}
+	
+	// Delete mapping to remove weapons
+	@DeleteMapping(path = "/{id}")
+	public ResponseEntity<?> deleteWeapon(@PathVariable(name = "id") int id) {
+		WeaponDTO deletedWeapon = weaponService.getWeapon(id);
+		weaponService.deleteWeapon(id);
+		return ResponseEntity.ok(deletedWeapon);
 	}
 }
