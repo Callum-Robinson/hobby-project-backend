bc61a7d0bf0ad62b5ebf8a013cc8ae536b8ed515
Callum <callumjrobinson2020@gmail.com>
2022-04-26 16:52:23 +0100

    added constructor and annotations to weapon controller

--- src/main/java/com/qa/hobbyprojectbackend/controllers/WeaponController.java
+++ src/main/java/com/qa/hobbyprojectbackend/controllers/WeaponController.java
@@ -1,5 +1,21 @@
 package com.qa.hobbyprojectbackend.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.hobbyprojectbackend.service.WeaponService;
+
+@RestController
+@RequestMapping(path = "/weapon")
+@CrossOrigin("*")
 public class WeaponController {
 
+	private WeaponService weaponService;
+	
+	@Autowired
+	public WeaponController(WeaponService weaponService) {
+		this.weaponService = weaponService;
+	}
 }
