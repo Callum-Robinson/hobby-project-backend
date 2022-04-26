47b2b7531dea9617a808f90a534fe86d78bc0f76
Callum <callumjrobinson2020@gmail.com>
2022-04-26 10:18:35 +0100

    added delete mapping to character controller

--- src/main/java/com/qa/hobbyprojectbackend/controllers/CharacterController.java
+++ src/main/java/com/qa/hobbyprojectbackend/controllers/CharacterController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -63,5 +64,13 @@ public class CharacterController {
 	public ResponseEntity<MyCharacterDTO> updateCharacter(@RequestBody UpdateCharacterDTO updateCharacterDTO, 
 			@PathVariable(name = "id") int id) {
 		return ResponseEntity.ok(characterService.updateCharacter(updateCharacterDTO, id));
+	}
+	
+	// delete mapping for deleting characters
+	@DeleteMapping(path = "/{id}")
+	public ResponseEntity<?> deleteCharacter(@PathVariable(name = "id") int id) {
+		MyCharacterDTO deletedCharacter = characterService.getCharacter(id);
+		characterService.deleteCharacter(id);
+		return ResponseEntity.ok(deletedCharacter);
 	}
 }
