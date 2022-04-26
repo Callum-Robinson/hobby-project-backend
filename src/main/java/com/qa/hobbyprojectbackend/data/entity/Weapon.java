a9824cf6577ad37b48224f970ce7a422e36b2c24
Callum <callumjrobinson2020@gmail.com>
2022-04-26 11:25:26 +0100

    created weapon entity class

---
+++ src/main/java/com/qa/hobbyprojectbackend/data/entity/Weapon.java
@@ -0,0 +1,17 @@
+package com.qa.hobbyprojectbackend.data.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "weapon")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Weapon {
+
+}
