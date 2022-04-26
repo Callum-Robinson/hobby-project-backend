3312c4a0e4fb320999139d2ccc003b38944e25de
Callum <callumjrobinson2020@gmail.com>
2022-04-26 14:55:58 +0100

    added toDTO method to weapon DTO

--- src/main/java/com/qa/hobbyprojectbackend/service/WeaponService.java
+++ src/main/java/com/qa/hobbyprojectbackend/service/WeaponService.java
@@ -1,13 +1,27 @@
 package com.qa.hobbyprojectbackend.service;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.qa.hobbyprojectbackend.data.entity.Weapon;
 import com.qa.hobbyprojectbackend.data.repository.WeaponRepository;
+import com.qa.hobbyprojectbackend.dto.WeaponDTO;
 
 @Service
 public class WeaponService {
 
 	private WeaponRepository weaponRepository;
 	private ModelMapper modelMapper;
+	
+	@Autowired
+	public WeaponService(WeaponRepository weaponRepository, ModelMapper modelMapper) {
+		super();
+		this.weaponRepository = weaponRepository;
+		this.modelMapper = modelMapper;
+	}
+	
+	private WeaponDTO toDTO(Weapon weapon) {
+		return modelMapper.map(weapon, WeaponDTO.class);
+	}
 }
