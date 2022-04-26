6fca03a789967fb9e626d7e8ed049bc965d20e32
Callum <callumjrobinson2020@gmail.com>
2022-04-26 17:26:00 +0100

    added get mappings to weapon controller

--- src/main/java/com/qa/hobbyprojectbackend/controllers/WeaponController.java
+++ src/main/java/com/qa/hobbyprojectbackend/controllers/WeaponController.java
@@ -1,10 +1,16 @@
 package com.qa.hobbyprojectbackend.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.qa.hobbyprojectbackend.dto.WeaponDTO;
 import com.qa.hobbyprojectbackend.service.WeaponService;
 
 @RestController
@@ -17,5 +23,17 @@ public class WeaponController {
 	@Autowired
 	public WeaponController(WeaponService weaponService) {
 		this.weaponService = weaponService;
+	}
+	
+	// Get all weapons mapping
+	@GetMapping
+	public ResponseEntity<List<WeaponDTO>> getWeapons() {
+		return ResponseEntity.ok(weaponService.getWeapons());
+	}
+	
+	// Get weapon by weapon id mapping
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<WeaponDTO> getWeapon(@PathVariable(name = "id") int id) {
+		return ResponseEntity.ok(weaponService.getWeapon(id));
 	}
 }
