fafdcf0329fa761417a6dc2131fb87e04d86ac32
Callum <callumjrobinson2020@gmail.com>
2022-04-26 16:40:42 +0100

    added sample weapons to dev startup listener

--- src/main/java/com/qa/hobbyprojectbackend/configuration/DevStartupListener.java
+++ src/main/java/com/qa/hobbyprojectbackend/configuration/DevStartupListener.java
@@ -9,17 +9,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.qa.hobbyprojectbackend.data.entity.MyCharacter;
+import com.qa.hobbyprojectbackend.data.entity.Weapon;
 import com.qa.hobbyprojectbackend.data.repository.CharacterRepository;
+import com.qa.hobbyprojectbackend.data.repository.WeaponRepository;
 
 @Profile("dev")
 @Configuration
 public class DevStartupListener implements ApplicationListener<ApplicationReadyEvent> {
 
 	private CharacterRepository characterRepository;
+	private WeaponRepository weaponRepository;
 	
 	@Autowired
-	public DevStartupListener(CharacterRepository characterRepository) {
+	public DevStartupListener(CharacterRepository characterRepository, WeaponRepository weaponRepository) {
 		this.characterRepository = characterRepository;
+		this.weaponRepository = weaponRepository;
 	}
 	
 	@Override
@@ -27,6 +31,15 @@ public class DevStartupListener implements ApplicationListener<ApplicationReadyE
 		List<MyCharacter> characters = characterRepository.saveAll(List.of(
 				new MyCharacter("Teoz", "Leonine", "", "Paladin", 5, "Oath of Vengeance", "Knight"),
 				new MyCharacter("Maria", "Tabaxi", "", "Rogue", 5, "Swashbuckler", "Sailor")
+				));
+		
+		MyCharacter character = characters.stream().filter(c -> c.getName().equals("Teoz")).findFirst().orElse(null);
+		
+		List<Weapon> weapons = weaponRepository.saveAll(List.of(
+				new Weapon("Manticore's Wrath", "Greataxe", "Martial Melee Weapon", "Very Rare", "30gp", "1d12 + 2", 
+						"Slashing", "Heavy, Two-Handed, Ranged", "+2 to attack rolls, ranged attack launching the spikes from the axe"),
+				new Weapon("Blackened Steel Heavy Hammer", "Maul", "Martial Melee Weapon", "Rare", "10gp", "2d6 + 1",
+						"Bludgeoning", "Heavy, Two-Handed", "+1 to hit and damage, has a ground attack")
 				));
 	}
 }
