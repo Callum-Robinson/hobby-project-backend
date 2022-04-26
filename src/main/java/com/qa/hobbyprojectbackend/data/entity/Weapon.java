1b414de8244d301090715fea9978c084555992e0
Callum <callumjrobinson2020@gmail.com>
2022-04-26 11:42:38 +0100

    added weapon fields to entity

--- src/main/java/com/qa/hobbyprojectbackend/data/entity/Weapon.java
+++ src/main/java/com/qa/hobbyprojectbackend/data/entity/Weapon.java
@@ -1,6 +1,10 @@
 package com.qa.hobbyprojectbackend.data.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -14,4 +18,26 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Weapon {
 
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	
+	private String base_weapon;
+	
+	private String weapon_type;
+	
+	private String rarity;
+	
+	private String cost;
+	
+	private String damage;
+	
+	private String damageType;
+	
+	private String properties;
+	
+	private String additional_abilities;
 }
