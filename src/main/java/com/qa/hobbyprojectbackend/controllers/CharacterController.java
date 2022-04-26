28304f5ee28c3e5a5654cf3442e4c371ad3fb1d7
Callum <callumjrobinson2020@gmail.com>
2022-04-26 09:36:35 +0100

    added read all get mapping to character controller

--- src/main/java/com/qa/hobbyprojectbackend/controllers/CharacterController.java
+++ src/main/java/com/qa/hobbyprojectbackend/controllers/CharacterController.java
@@ -1,10 +1,14 @@
 package com.qa.hobbyprojectbackend.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.qa.hobbyprojectbackend.dto.MyCharacterDTO;
 import com.qa.hobbyprojectbackend.service.CharacterService;
 
 @RestController
@@ -17,5 +21,10 @@ public class CharacterController {
 	@Autowired
 	public CharacterController(CharacterService characterService) {
 		this.characterService = characterService;
+	}
+	
+	// get mapping for read all
+	public ResponseEntity<List<MyCharacterDTO>> getCharacters() {
+		return ResponseEntity.ok(characterService.getCharacters());
 	}
 }
