0ec0e3936e7fc18aa8b072f6992a429dce52d9ce
Callum <callumjrobinson2020@gmail.com>
2022-04-26 17:34:43 +0100

    added post mapping to weapon controller

--- src/main/java/com/qa/hobbyprojectbackend/controllers/WeaponController.java
+++ src/main/java/com/qa/hobbyprojectbackend/controllers/WeaponController.java
@@ -2,14 +2,21 @@ package com.qa.hobbyprojectbackend.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.qa.hobbyprojectbackend.dto.NewWeaponDTO;
 import com.qa.hobbyprojectbackend.dto.WeaponDTO;
 import com.qa.hobbyprojectbackend.service.WeaponService;
 
@@ -36,4 +43,17 @@ public class WeaponController {
 	public ResponseEntity<WeaponDTO> getWeapon(@PathVariable(name = "id") int id) {
 		return ResponseEntity.ok(weaponService.getWeapon(id));
 	}
+	
+	// Post mapping for creating weapons
+	@PostMapping
+	public ResponseEntity<WeaponDTO> createWeapon(@Valid @RequestBody NewWeaponDTO weapon) {
+		WeaponDTO newWeapon = weaponService.createWeapon(weapon);
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Location", "http://localhost:8080/weapon/" + newWeapon.getId());
+		
+		return new ResponseEntity<>(newWeapon, headers, HttpStatus.CREATED);
+	}
+	
+	
 }
