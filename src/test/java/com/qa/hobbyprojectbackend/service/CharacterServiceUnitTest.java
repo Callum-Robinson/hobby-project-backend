ded7a8781a380533784549278d8c4dee17ed2eb6
Callum <callumjrobinson2020@gmail.com>
2022-04-27 10:01:41 +0100

    added mocks to character service unit test

--- src/test/java/com/qa/hobbyprojectbackend/service/CharacterServiceUnitTest.java
+++ src/test/java/com/qa/hobbyprojectbackend/service/CharacterServiceUnitTest.java
@@ -1,5 +1,22 @@
 package com.qa.hobbyprojectbackend.service;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+
+import com.qa.hobbyprojectbackend.data.repository.CharacterRepository;
+
+@ExtendWith(MockitoExtension.class)
 public class CharacterServiceUnitTest {
 
+	@Mock
+	private CharacterRepository characterRepository;
+	
+	@Mock
+	private ModelMapper modelMapper;
+	
+	@InjectMocks
+	private CharacterService characterService;
 }
