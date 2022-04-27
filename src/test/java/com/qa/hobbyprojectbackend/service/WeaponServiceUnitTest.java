88198bbcfe266242bb2e9b316f65ae40b9cdff49
Callum <callumjrobinson2020@gmail.com>
2022-04-27 16:15:07 +0100

    setup up weapon service unit test

--- src/test/java/com/qa/hobbyprojectbackend/service/WeaponServiceUnitTest.java
+++ src/test/java/com/qa/hobbyprojectbackend/service/WeaponServiceUnitTest.java
@@ -1,10 +1,19 @@
 package com.qa.hobbyprojectbackend.service;
 
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 
+import com.qa.hobbyprojectbackend.data.entity.MyCharacter;
+import com.qa.hobbyprojectbackend.data.entity.Weapon;
 import com.qa.hobbyprojectbackend.data.repository.WeaponRepository;
+import com.qa.hobbyprojectbackend.dto.MyCharacterDTO;
+import com.qa.hobbyprojectbackend.dto.WeaponDTO;
 
 @ExtendWith(MockitoExtension.class)
 public class WeaponServiceUnitTest {
@@ -12,5 +21,35 @@ public class WeaponServiceUnitTest {
 	@Mock
 	private WeaponRepository weaponRepository;
 	
-	//@Mock
+	@Mock
+	private ModelMapper modelMapper;
+	
+	@InjectMocks
+	private WeaponService weaponService;
+	
+	private List<MyCharacter> characters;
+	private List<MyCharacterDTO> characterDTOs;
+	private List<Weapon> weapons;
+	private List<WeaponDTO> weaponDTOs;
+	
+	@BeforeEach
+	public void init() {
+		characters = List.of(
+				new MyCharacter(1, "Teoz", "Leonine", "", "Paladin", 5, "Oath of Vengeance", "Knight"),
+				new MyCharacter(2, "Maria", "Tabaxi", "", "Rogue", 5, "Swashbuckler", "Sailor"));
+		characterDTOs = List.of(
+				new MyCharacterDTO(1, "Teoz", "Leonine", "", "Paladin", 5, "Oath of Vengeance", "Knight"),
+				new MyCharacterDTO(2, "Maria", "Tabaxi", "", "Rogue", 5, "Swashbuckler", "Sailor"));
+		
+		weapons = List.of(
+				new Weapon(1, characters.get(0), "Manticore's Wrath", "Greataxe", "Martial Melee Weapon", "Very Rare", "30gp", "1d12 + 2", 
+				"Slashing", "Heavy, Two-Handed, Ranged", "+2 to attack rolls, ranged attack launching the spikes from the axe"),
+				new Weapon(2, characters.get(0), "Blackened Steel Heavy Hammer", "Maul", "Martial Melee Weapon", "Rare", "10gp", "2d6 + 1",
+				"Bludgeoning", "Heavy, Two-Handed", "+1 to hit and damage, has a ground attack"));
+		weaponDTOs = List.of(
+				new WeaponDTO(1, characterDTOs.get(0), "Manticore's Wrath", "Greataxe", "Martial Melee Weapon", "Very Rare", "30gp", "1d12 + 2", 
+				"Slashing", "Heavy, Two-Handed, Ranged", "+2 to attack rolls, ranged attack launching the spikes from the axe"),
+				new WeaponDTO(2, characterDTOs.get(0), "Blackened Steel Heavy Hammer", "Maul", "Martial Melee Weapon", "Rare", "10gp", "2d6 + 1",
+				"Bludgeoning", "Heavy, Two-Handed", "+1 to hit and damage, has a ground attack"));
+	}
 }
